6298889d60a0c9e52d2a3c300860f286bbf982e0
ismaelforj <ismael@forj.network>
2023-02-24 18:28:55 -0300

    Updates

---
+++ precache-manifest.d83a190c23108c32672653aeab6641fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b298f6d0d4d02d241a61108c53f8f621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3579fde0b60236633157",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "c7ef849be905fceacf47",
+    "url": "/static/css/main.9d0f3485.chunk.css"
+  },
+  {
+    "revision": "3579fde0b60236633157",
+    "url": "/static/js/2.21cf0d30.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.21cf0d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ef849be905fceacf47",
+    "url": "/static/js/main.1117c519.chunk.js"
+  },
+  {
+    "revision": "f4b4b3e4a9a9393d112c",
+    "url": "/static/js/runtime-main.0d99b6fc.js"
+  },
+  {
+    "revision": "fa96eac9576a9122a2f06977710234ec",
+    "url": "/static/media/castle.fa96eac9.png"
+  },
+  {
+    "revision": "c0838d86f80431eb737e08b58629a5df",
+    "url": "/static/media/diamond.c0838d86.jpg"
+  },
+  {
+    "revision": "550e42729af353444fdc00713940a756",
+    "url": "/static/media/free-space-grass.550e4272.png"
+  },
+  {
+    "revision": "d6e62841849c8205a7e5b645ae3799ae",
+    "url": "/static/media/main-back-image.d6e62841.jpg"
+  },
+  {
+    "revision": "1d17600432b697b26d01f453e1c68901",
+    "url": "/static/media/star.1d176004.jpg"
+  },
+  {
+    "revision": "88442141c181d765da17e1a27e3df1da",
+    "url": "/static/media/wall-brick.88442141.png"
+  }
+]);
